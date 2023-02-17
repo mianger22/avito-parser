45e0b1ce47908d711b74df010041278f35f2b27f
BossNumber1 <dimaang23@gmail.com>
2023-02-17 12:39:37 +0300

    upgrade code

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,19 @@ import { clear, error, log } from "console";
 import jsdom from "jsdom";
 const {JSDOM} = jsdom;
 
-(async ()=> {
+(async () => {
   let html: string;
   try {
-    // const url = "https://www.avito.ru/moskva/telefony?q=airtag&s=104";
-    const url = "https://www.avito.ru/velikiy_novgorod/kvartiry/sdam/na_dlitelnyy_srok/1-komnatnye-ASgBAQICAkSSA8gQ8AeQUgFAzAgUjlk?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA&localPriority=0";
-    const resp = await axios.get(url);
-    
+    // формируем запрос получения всех объявлений по сдаваемым квартирам
+    const baseUrl = "https://www.avito.ru/velikiy_novgorod/kvartiry/sdam/na_dlitelnyy_srok/1-komnatnye-ASgBAQICAkSSA8gQ8AeQUgFAzAgUjlk?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA&localPriority=0&p=";
+    // применяем его к определённой странице
+    const urlByPages: string = baseUrl + 1;
+    // получаем все объявления
+    const resp = await axios.get(urlByPages);
+    // сохраняем их в переменную
     html = resp.data;
   } catch (e) {
+    // если возникли ошибки
       if (axios.isAxiosError(e)) {
         error(e);
       } else {
@@ -22,11 +26,11 @@ const {JSDOM} = jsdom;
   const dom = new JSDOM(html);
   const document = dom.window.document;
   const items = document.querySelectorAll('[data-marker=item]');
-
-  clear();
-
+  
+  // будем хранить ссылки, заголовки и цены
   const newAds: any = {};
 
+  // получаем нужные данные
   items.forEach(node => {
     newAds[node.id] = {
       url: node.querySelector('[itemprop=url]').getAttribute('href'),
@@ -35,14 +39,7 @@ const {JSDOM} = jsdom;
     }
   })
 
-  // items.forEach(node => {
-  //   newAds[node.id] = {
-  //     id: node.id,
-  //     url: node.querySelector('[itemprop=url]').getAttribute('href'),
-  //     title: node.querySelector('[itemprop=name]').textContent,
-  //     price: Number(node.querySelector('[itemprop=price]').getAttribute('content'))
-  //   }
-  // })
-
-  log("newAds:", newAds); 
+  // показываем в консоли результат
+  clear();
+  log("result:", newAds); 
 })()
