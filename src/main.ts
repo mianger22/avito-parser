b402e9b57e29f33b6877b57ad5f89c0a37bf5440
BossNumber1 <dimaang23@gmail.com>
2023-03-07 19:51:23 +0300

    started do parsing cian

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,18 @@ const {JSDOM} = jsdom;
 let numberPages: number = 2;
 numberPages++;
 // запрос получения всех объявлений по сдаваемым квартирам
-const baseUrl = "https://www.avito.ru/velikiy_novgorod/kvartiry/sdam/na_dlitelnyy_srok/1-komnatnye-ASgBAQICAkSSA8gQ8AeQUgFAzAgUjlk?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA&localPriority=0&p=";
+// const baseUrl0 = "https://www.avito.ru/velikiy_novgorod/kvartiry/sdam/na_dlitelnyy_srok/1-komnatnye-ASgBAQICAkSSA8gQ8AeQUgFAzAgUjlk?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA&localPriority=0&p=";
+
+// const baseUrl1 = "https://novgorod.cian.ru/snyat-kvartiru/";
+const baseUrlPartFirst = "https://novgorod.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&p=";
+const baseUrlPartSecond = "&region=4694&type=4";
 
 async function findApartments(pageNumber: number) {
   let html: string;
   
   try {
     // применяем его к определённой странице
-    const urlByPages: string = baseUrl + pageNumber;
+    const urlByPages: string = baseUrlPartFirst + pageNumber + baseUrlPartSecond;
     // получаем все объявления
     const resp = await axios.get(urlByPages);
     // сохраняем их в переменную
@@ -31,26 +35,30 @@ async function findApartments(pageNumber: number) {
   // приводим данные с Авито к удобному для работы виду
   const dom = new JSDOM(html);
   const document = dom.window.document;
-  const items = document.querySelectorAll('[data-marker=item]');
+
+  console.clear();
+  console.log("document = ", document);
+
+  // const items = document.querySelectorAll('[data-marker=item]');
 
   // будем хранить ссылки, заголовки и цены
   const newAds: any = [];
 
   // получаем нужные данные
-  items.forEach(node => {  
-    const content = node.textContent;   
+  // items.forEach(node => {  
+  //   const content = node.textContent;   
 
-    // получаем ссылку на объявление
-    newAds.push({
-      id: node.id,
-      url: 'https://avito.ru/' + node.querySelector('[itemprop=url]').getAttribute('href'),
-      title: node.querySelector('[itemprop=name]').textContent,
-      price: Number(node.querySelector('[itemprop=price]').getAttribute('content')),
-      realtor: content.indexOf('Агентство') != -1 ? true : false,
-      rooms: content.indexOf('1-к.') != -1 ? 1 : content.indexOf('2-к.') != -1 ? 2 : 3,
-      collateral: content.indexOf('залога') != -1 ? false : true
-    })
-  })
+  //   // получаем ссылку на объявление
+  //   newAds.push({
+  //     id: node.id,
+  //     url: 'https://avito.ru/' + node.querySelector('[itemprop=url]').getAttribute('href'),
+  //     title: node.querySelector('[itemprop=name]').textContent,
+  //     price: Number(node.querySelector('[itemprop=price]').getAttribute('content')),
+  //     realtor: content.indexOf('Агентство') != -1 ? true : false,
+  //     rooms: content.indexOf('1-к.') != -1 ? 1 : content.indexOf('2-к.') != -1 ? 2 : 3,
+  //     collateral: content.indexOf('залога') != -1 ? false : true
+  //   })
+  // })
 
   // отдаём браузеру результат
   return newAds;
